c2ac43aed67b4ffef4f2a4b5721809bf3622b895
luyanbo <robert.lyb@alibaba-inc.com>
2023-03-15 17:53:54 +0800

    update dependencies

--- src/loader/load_elf.rs
+++ src/loader/load_elf.rs
@@ -1,45 +1,32 @@
-
-
-extern crate nix;
 extern crate libc;
-use nix::sys::mman::{
-    mmap,
-    mprotect,
-    ProtFlags,
-    MapFlags
-};
+extern crate nix;
 use core::ffi::c_void;
+use nix::sys::mman::{mmap, mprotect, MapFlags, ProtFlags};
+use std::num::NonZeroUsize;
 
-
-use crate::parse_elf::{Elf64Phdr, LoadInfo, ElfType};
+use crate::parse_elf::{Elf64Phdr, ElfType, LoadInfo};
 
 // these values are used to translate ElfPhdr64
 const PF_X: u32 = 1;
 const PF_W: u32 = 2;
 const PF_R: u32 = 4;
 
-
 const ELF_MIN_ALIGNMENT: usize = 0x1000;
 const ELF_MIN_ALIGNMENT_MASK: usize = !(ELF_MIN_ALIGNMENT - 1);
-
-
 
 /// represents a PT_LOAD segment of the ELF to be loaded and holds the data to be loaded
 #[derive(Debug, Clone)]
 pub struct ElfSegment {
-    pub virt_addr:  usize,
-    pub memsize:    usize,
-    pub offset:     usize,
-    filesize:       usize,
-    alignment:      usize,
-    data:           Vec<u8>,
-    prot:           ProtFlags,
+    pub virt_addr: usize,
+    pub memsize: usize,
+    pub offset: usize,
+    filesize: usize,
+    alignment: usize,
+    data: Vec<u8>,
+    prot: ProtFlags,
 }
 
-
-
 impl ElfSegment {
-
     /// takes a raw Elf64Phdr as parsed by the parsing module and converts
     /// it into a loadable segment
     pub fn new(hdr: &Elf64Phdr, data: Vec<u8>) -> Self {
@@ -72,7 +59,6 @@ impl ElfSegment {
     }
 }
 
-
 /// Statefully emulates the Linux kernel ELF loading logic
 pub struct ElfLoad {
     pub load_addr: usize,
@@ -89,31 +75,31 @@ impl ElfLoad {
     pub fn load(load_info: &LoadInfo) -> Self {
         /* There are two types of ELF files:
         *  ET_EXEC and ET_DYN. ET_EXEC are position dependent and are given a load address by the compiler (for gcc it is usually 0x40000)
-        *  In the case of such an executable, simply obtain the virtual address of the first PT_LOAD program header and use it as 
-        *  an address for a MAP_FIXED mmap() mapping. 
+        *  In the case of such an executable, simply obtain the virtual address of the first PT_LOAD program header and use it as
+        *  an address for a MAP_FIXED mmap() mapping.
 
         *  In case of an ET_DYN ELF, the code is position independent and can be loaded anywhere. The Linux kernel usually chooses
         *  0x555555554aaa + ASLR offset. However, since we are already loaded at that address we just let mmap() chose a suitable location
         *  for the new binary. This can be a little awkward as mmap() chooses an address in the 0x7fff... range and might map the file next
-        *  to another file. This is awkward because the libc heap uses brk() (the end of the loaded program) to initialize the heap. 
+        *  to another file. This is awkward because the libc heap uses brk() (the end of the loaded program) to initialize the heap.
         *  Therefor, we need to set brk() to a safe location where the heap can grow.
 
         The corresponding Linux kernel code of the following logic is:
 
-        	if (interpreter) {
-				load_bias = ELF_ET_DYN_BASE;
-				if (current->flags & PF_RANDOMIZE)
-					load_bias += arch_mmap_rnd();
-				alignment = maximum_alignment(elf_phdata, elf_ex->e_phnum);
-				if (alignment)
-					load_bias &= ~(alignment - 1);
+            if (interpreter) {
+                load_bias = ELF_ET_DYN_BASE;
+                if (current->flags & PF_RANDOMIZE)
+                    load_bias += arch_mmap_rnd();
+                alignment = maximum_alignment(elf_phdata, elf_ex->e_phnum);
+                if (alignment)
+                    load_bias &= ~(alignment - 1);
                 elf_flags |= MAP_FIXED;
             else
                 load_bias = 0
 
 
-            The kernel then loads each segment through load_bias + vaddr of section. This way, 
-            both static binaries (load bias of 0) that have a set load address and PIE binaries can be loaded with 
+            The kernel then loads each segment through load_bias + vaddr of section. This way,
+            both static binaries (load bias of 0) that have a set load address and PIE binaries can be loaded with
             the same logic.
         */
 
@@ -129,46 +115,57 @@ impl ElfLoad {
             ElfType::ElfExec => {
                 map_flags.insert(MapFlags::MAP_FIXED);
                 segments[0].virt_addr - segments[0].offset
-            },
-            ElfType::ElfDyn => 0
+            }
+            ElfType::ElfDyn => 0,
         };
 
         let map_flags = map_flags;
 
-        // make an allocation large enough for the entire ELF binary, make it read and writable 
+        // make an allocation large enough for the entire ELF binary, make it read and writable
         // and populate it with the content. Then change the protection flags for each segment accordingly.
-        let total_mapping_size = Self::get_total_mapping_size(&segments);  
+        let total_mapping_size = Self::get_total_mapping_size(&segments);
         let mut prot_flags = ProtFlags::empty();
         prot_flags.insert(ProtFlags::PROT_READ);
         prot_flags.insert(ProtFlags::PROT_WRITE);
-        
+
         let load_addr = unsafe {
-            mmap(mmap_addr as *mut c_void, total_mapping_size, prot_flags, map_flags, -1, 0).expect("Failed to map!")
+            mmap(
+                NonZeroUsize::new(mmap_addr),
+                NonZeroUsize::new(total_mapping_size).expect("total_mapping_size is illegal"),
+                prot_flags,
+                map_flags,
+                -1,
+                0,
+            )
+            .expect("Failed to map!")
         };
 
-        // use the load_addr to offset the virt address of each segment. If this is a 
+        // use the load_addr to offset the virt address of each segment. If this is a
         // static binary, set it to 0 since the virtual addresses in the headers are absolute
-        let load_base = if mmap_addr == 0 { 
+        let load_base = if mmap_addr == 0 {
             load_addr as usize
-        } else { 
+        } else {
             0
         };
 
         // populate the pages of each segment and change the protection status
         for seg in segments.iter() {
-
             // this is the same logic as in the Linux kernel for aligning addresses of
             // program headers
             let addr = load_base + seg.virt_addr;
-            let size = seg.filesize + (addr & (ELF_MIN_ALIGNMENT -1));
+            let size = seg.filesize + (addr & (ELF_MIN_ALIGNMENT - 1));
 
             let addr = addr & ELF_MIN_ALIGNMENT_MASK;
             let size = (size + ELF_MIN_ALIGNMENT - 1) & ELF_MIN_ALIGNMENT_MASK;
-            
+
             // copy the actual number of bytes in the file. The (size) might be larger than this
             // as the segment might contain uninitialized data
             unsafe {
-                libc::memcpy(addr as *mut c_void, seg.data.as_ptr() as *const c_void, seg.data.len());
+                libc::memcpy(
+                    addr as *mut c_void,
+                    seg.data.as_ptr() as *const c_void,
+                    seg.data.len(),
+                );
             }
 
             // now change the protection of the segment (with the actual size)
@@ -181,6 +178,4 @@ impl ElfLoad {
             load_addr: load_addr as usize,
         }
     }
-
-
 }
--- src/loader/stack_setup.rs
+++ src/loader/stack_setup.rs
@@ -1,50 +1,39 @@
-
 use core::ffi::c_void;
+use std::num::NonZeroUsize;
 
 extern crate rand;
 use rand::Rng;
 
 extern crate nix;
-use nix::sys::mman::{
-    mmap,
-    ProtFlags,
-    MapFlags
-};
+use nix::sys::mman::{mmap, MapFlags, ProtFlags};
 
 extern crate libc;
 
-use crate::parse_elf::{
-    LoadInfo,
-    ElfType
-};
-
-
+use crate::parse_elf::{ElfType, LoadInfo};
 
 /// ELFAux IDs and values
 const AT_SYSINFO_EHDR: u64 = 33;
-const AT_HWCAP: u64 =   16;
-const AT_PAGESZ: u64 =  6;
-const AT_CLKTCK: u64 =  17;
-const AT_PHDR: u64 =    3;
-const AT_PHENT: u64 =   4;
-const AT_PHNUM: u64 =   5;
-const AT_BASE: u64 =   7;
-const AT_FLAGS: u64 =   8;
-const AT_ENTRY: u64 =   9;
-const AT_UID: u64 =   11;
-const AT_EUID: u64 =   12;
-const AT_GID: u64 =   13;
-const AT_EGID: u64 =   14;
-const AT_SECURE: u64 =   23;
-const AT_RANDOM: u64 =   25;
-const AT_HWCAP2: u64 =   26;
-const AT_EXECFN: u64 =   31;
-const AT_NULL: u64 =   0;
-
+const AT_HWCAP: u64 = 16;
+const AT_PAGESZ: u64 = 6;
+const AT_CLKTCK: u64 = 17;
+const AT_PHDR: u64 = 3;
+const AT_PHENT: u64 = 4;
+const AT_PHNUM: u64 = 5;
+const AT_BASE: u64 = 7;
+const AT_FLAGS: u64 = 8;
+const AT_ENTRY: u64 = 9;
+const AT_UID: u64 = 11;
+const AT_EUID: u64 = 12;
+const AT_GID: u64 = 13;
+const AT_EGID: u64 = 14;
+const AT_SECURE: u64 = 23;
+const AT_RANDOM: u64 = 25;
+const AT_HWCAP2: u64 = 26;
+const AT_EXECFN: u64 = 31;
+const AT_NULL: u64 = 0;
 
 /// the standard size of a program header and the only one we support
 const PHENT_SIZE: usize = 0x38;
-
 
 /// sets up an initial stack according to the System-V x86 ABI and passes information such
 /// as the entry point and location of the program headers of the application to tbe loaded
@@ -61,7 +50,6 @@ pub fn setup_stack(load_info: &LoadInfo, load_address: usize, interp_base: usize
 
         map_flags
     };
-
 
     let stack_prot = {
         let mut prot_flags = ProtFlags::empty();
@@ -100,7 +88,6 @@ pub fn setup_stack(load_info: &LoadInfo, load_address: usize, interp_base: usize
         write_data(stack_pointer, &env_var.as_bytes())
     }
 
-    
     // copy the contents of the program arguments onto the stack and build an argv[] pointer array
     // ignore argv[1], as it is the program to be loaded and is interpreted by this program itself
     let mut argv: Vec<usize> = Vec::new();
@@ -109,29 +96,24 @@ pub fn setup_stack(load_info: &LoadInfo, load_address: usize, interp_base: usize
     for arg in args.iter_mut().rev() {
         stack_pointer -= arg.len() + 1; // +1 for a NULLBYTE
         argv.push(stack_pointer);
-        let argv_bytes = unsafe {
-            arg.as_mut_vec()
-        };
+        let argv_bytes = unsafe { arg.as_mut_vec() };
         argv_bytes.push(0); // push a nullbyte
         write_data(stack_pointer, &argv_bytes);
     }
 
-
     // after copying the argument contents and environment variables, 16 byte align the stack pointer
     stack_pointer &= !0xf;
-
 
     // place the platform string on the stack
     stack_pointer -= "x86_64".len() + 1;
     write_data(stack_pointer, "x86_64\0".as_bytes());
-    
+
     // the next item are 16bytes of random data as a PRNG seed
     let seed_bytes = rand::thread_rng().gen::<[u8; 16]>();
     stack_pointer -= seed_bytes.len();
     write_data(stack_pointer, &seed_bytes);
     let prng_pointer = stack_pointer;
 
-    
     // allocate space for the AUX vectors
     stack_pointer -= 0x120;
 
@@ -145,7 +127,6 @@ pub fn setup_stack(load_info: &LoadInfo, load_address: usize, interp_base: usize
     // the current stack pointer is the one we will return!
     let rsp = stack_pointer;
 
-
     // put argc on the stack
     write_pointer(&mut stack_pointer, argv.len());
 
@@ -157,7 +138,6 @@ pub fn setup_stack(load_info: &LoadInfo, load_address: usize, interp_base: usize
     // place a NULL pointer to signal this is the end of the argv array
     write_pointer(&mut stack_pointer, 0x0);
 
-
     // write each of the environemnt pointers to the stack
     for e in env.iter() {
         write_pointer(&mut stack_pointer, *e);
@@ -166,23 +146,29 @@ pub fn setup_stack(load_info: &LoadInfo, load_address: usize, interp_base: usize
     // place a NULL pointer to signify that this is the end of the environment pointer
     write_pointer(&mut stack_pointer, 0x0);
 
-
-        // next are the AUX information needed for the ELF Interpreter and/or __libc_start_main
+    // next are the AUX information needed for the ELF Interpreter and/or __libc_start_main
     // we can derive most of them via libc's getauxval()
     unsafe {
-
         // VDSO is a shared object mapped into userspace by the kernel that can be used by libc
-        write_aux_val(&mut stack_pointer, AT_SYSINFO_EHDR, libc::getauxval(AT_SYSINFO_EHDR));
+        write_aux_val(
+            &mut stack_pointer,
+            AT_SYSINFO_EHDR,
+            libc::getauxval(AT_SYSINFO_EHDR),
+        );
 
         // some generic architecture / processor specific value we derive from our own auxvector
         write_aux_val(&mut stack_pointer, AT_HWCAP, libc::getauxval(AT_HWCAP));
         write_aux_val(&mut stack_pointer, AT_PAGESZ, libc::getauxval(AT_PAGESZ));
-        write_aux_val(&mut stack_pointer, AT_CLKTCK, libc::getauxval(AT_CLKTCK)); 
+        write_aux_val(&mut stack_pointer, AT_CLKTCK, libc::getauxval(AT_CLKTCK));
         write_aux_val(&mut stack_pointer, AT_HWCAP2, libc::getauxval(AT_HWCAP2));
 
         // tell the CSU where to find the program headers of the binary to be loaded
         // to do this, we pass a pointer to them, the size of an entry and the number of entries
-        write_aux_val(&mut stack_pointer, AT_PHDR, (load_address + load_info.pheader_off) as u64);
+        write_aux_val(
+            &mut stack_pointer,
+            AT_PHDR,
+            (load_address + load_info.pheader_off) as u64,
+        );
         write_aux_val(&mut stack_pointer, AT_PHENT, PHENT_SIZE as u64);
         write_aux_val(&mut stack_pointer, AT_PHNUM, load_info.pheader_num as u64);
 
@@ -192,14 +178,19 @@ pub fn setup_stack(load_info: &LoadInfo, load_address: usize, interp_base: usize
         // the flags are hardcoded 0 by the kernel
         write_aux_val(&mut stack_pointer, AT_FLAGS, 0x0);
 
-        // the entry point of this binary. It is used by (ld.so) to jump to the binary once relocations 
+        // the entry point of this binary. It is used by (ld.so) to jump to the binary once relocations
         // have been performed
         // this might be relative or absolute, dependeing on the type of binary that is loaded!
         match load_info.etype {
-            ElfType::ElfExec => write_aux_val(&mut stack_pointer, AT_ENTRY, load_info.entry_point as u64),
-            ElfType::ElfDyn => write_aux_val(&mut stack_pointer, AT_ENTRY, (load_address + load_info.entry_point) as u64)
+            ElfType::ElfExec => {
+                write_aux_val(&mut stack_pointer, AT_ENTRY, load_info.entry_point as u64)
+            }
+            ElfType::ElfDyn => write_aux_val(
+                &mut stack_pointer,
+                AT_ENTRY,
+                (load_address + load_info.entry_point) as u64,
+            ),
         }
-        
 
         // pass some generic info about the user running the process deriving from our own auxval
         write_aux_val(&mut stack_pointer, AT_UID, libc::getauxval(AT_UID));
@@ -210,7 +201,6 @@ pub fn setup_stack(load_info: &LoadInfo, load_address: usize, interp_base: usize
 
         // pass a pointer to the initial 16 bytes of random memory for use by libc
         write_aux_val(&mut stack_pointer, AT_RANDOM, prng_pointer as u64);
-        
 
         // store a pointer to the program name here
         write_aux_val(&mut stack_pointer, AT_EXECFN, argv[0] as u64);
@@ -218,32 +208,30 @@ pub fn setup_stack(load_info: &LoadInfo, load_address: usize, interp_base: usize
         // end the aux vector
         write_aux_val(&mut stack_pointer, AT_NULL, 0x0);
     };
-    
+
     // that's it! We should now have a valid and clean stack for executing the new program
     // return the current stack pointer so that we can return it!
     rsp
 }
 
-
-
 fn write_data(sp: usize, data: &[u8]) {
     unsafe {
-        libc::memcpy(sp as *mut c_void, data.as_ptr() as *const c_void, data.len());
+        libc::memcpy(
+            sp as *mut c_void,
+            data.as_ptr() as *const c_void,
+            data.len(),
+        );
     }
 }
 
 // writes an AUX val to the stack and advances the stack pointer
 fn write_aux_val(sp: &mut usize, aux_id: u64, aux_val: u64) {
-   
     write_pointer(sp, aux_id as usize);
     write_pointer(sp, aux_val as usize);
-
 }
 
 fn write_pointer(sp: &mut usize, value: usize) {
     let ptr = *sp as *mut usize;
-    unsafe {
-        *ptr = value
-    };
+    unsafe { *ptr = value };
     *sp += 8;
 }
